fb65fe995c574af8234146bb0c2d0e6d4ca8a082
Sohan Patil <sohanpatil@outlook.com>
2019-09-05 17:46:51 -0400

    Added sitemap mapping

---
+++ gowebapp.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type Sitemapindex struct {
+	Sitemaps []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Location []string `xml:"url>loc"`
+	Title    []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+}
+
+type NewsMap struct {
+	Location string
+	Keywords string
+}
+
+func main() {
+
+	var newsData News
+	var sms Sitemapindex
+	newsMap := make(map[string]NewsMap)
+
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &sms)
+
+	for _, biner := range sms.Sitemaps {
+		resp, _ := http.Get(strings.TrimSpace(biner))
+		bytes, _ := ioutil.ReadAll(resp.Body)
+
+		//unmarshall all bytes/news
+		xml.Unmarshal(bytes, &newsData)
+		for idx := range newsData.Keywords {
+			newsMap[newsData.Title[idx]] = NewsMap{newsData.Keywords[idx], newsData.Location[idx]}
+		}
+		resp.Body.Close()
+		fmt.Println(newsMap)
+
+	}
+
+	resp.Body.Close()
+}
